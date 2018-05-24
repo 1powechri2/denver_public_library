686d1f1e156aa59bfe3d29a16120e90366863b93
Chris Powell <chris.ian.powell@gmail.com>
2018-05-24 09:28:20 -0600

    book class and tests underway

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,9 @@
 class Book
+  def initialize(book)
+    @book = book
+  end
 
-
+  def author_first_name
+    @book[:author_first_name]
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,19 @@
 require './test/test_helper'
+require './lib/book'
 
 class BookTest < Minitest::Test
+  def setup
+      @book = Book.new({author_first_name: "Harper",\
+                       author_last_name: "Lee",\
+                       title: "To Kill a Mockingbird",\
+                       publication_date: "July 11, 1960"})
+  end
 
+  def test_book_exists
+    assert_instance_of Book, @book
+  end
 
+  def test_author_first_name
+    assert_equal 'Harper', @book.author_first_name
+  end
 end
