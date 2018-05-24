6dd362ebeb578b6fbf325b030c2ed501d6c0f274
Chris Powell <chris.ian.powell@gmail.com>
2018-05-24 10:27:56 -0600

    author tests and class finished

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,20 @@
-class Author
+require './lib/book'
 
+class Author
+  attr_reader :books
+
+  def initialize(author)
+    @first_name = author[:first_name]
+    @last_name = author[:last_name]
+    @books = []
+  end
+
+  def add_book(title, pub_date)
+    book = Book.new({author_first_name:@first_name,\
+                        author_last_name:@last_name,
+                        title:title,\
+                        publication_date:pub_date})
+    @books.push(book)
+    return book
+  end
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,31 @@
 require './test/test_helper'
+require './lib/author'
 
 class AuthorTest < Minitest::Test
+  def setup
+    @charlotte_bronte = Author.new({first_name: "Charlotte",\
+                                   last_name: "Bronte"})
+  end
 
+  def test_author_exits
+    assert_instance_of Author, @charlotte_bronte
+  end
 
+  def test_author_has_no_books
+    assert_equal [], @charlotte_bronte.books
+  end
+
+   def test_add_book
+     book = @charlotte_bronte.add_book("Jane Eyre", "1847")
+     assert_equal book, @charlotte_bronte.books[0]
+   end
+
+   def test_add_multiple_books
+     book_1 = @charlotte_bronte.add_book("Jane Eyre", "1847")
+     book_2 = @charlotte_bronte.add_book("Villette", "1853")
+
+     assert_equal book_1, @charlotte_bronte.books[0]
+     assert_equal book_2, @charlotte_bronte.books[1]
+
+   end
 end
