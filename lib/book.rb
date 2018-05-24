0935be1729c709d83021def7a524e8923d15e9b5
Chris Powell <chris.ian.powell@gmail.com>
2018-05-24 09:36:46 -0600

    finished book tests and class

--- lib/book.rb
+++ lib/book.rb
@@ -6,4 +6,16 @@ class Book
   def author_first_name
     @book[:author_first_name]
   end
+
+  def author_last_name
+    @book[:author_last_name]
+  end
+
+  def title
+    @book[:title]
+  end
+
+  def publication_date
+    @book[:publication_date][-4..-1]
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -16,4 +16,16 @@ class BookTest < Minitest::Test
   def test_author_first_name
     assert_equal 'Harper', @book.author_first_name
   end
+
+  def test_author_last_name
+    assert_equal 'Lee', @book.author_last_name
+  end
+
+  def test_title
+    assert_equal 'To Kill a Mockingbird', @book.title
+  end
+
+  def test_publication_date
+    assert_equal '1960', @book.publication_date
+  end
 end
